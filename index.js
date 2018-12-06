22fee41c95c6f305afb3e72bbc03d0456355206a
no <no>
2018-12-06 13:42:04 +0300

    2

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const TOKEN = process.env.TLGTOKEN
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.onText(/\/mark/, (msg, match) => {
-	bot.sendMessage(msg.chat.id , `спасибо,Иван Васильевич`);
-	bot.sendMessage(`@anatoliygucci` , `я получил оценку`)
+	bot.sendMessage(msg.chat.id , `thank you ,Ivan Vasilyevich`);
+	bot.sendMessage(`@antoliygucci` , `Igot a mark`)
 })
 
 bot.on('message', msg => {
