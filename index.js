954c31c6e88ec3ce01c24dcaf356665fbc599e39
no <no@no.no>
2019-04-04 16:24:00 +0400

    dsws

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ bot.on('message', msg => {
 bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
 	bot.sendMessage(msg.chat.id, `Давление Р=F/S`)
 })
+
+bot.onText(/(формул)|(скажи).плотн*, (msg, match) => {
+	bot.sendMessage(msg.chat.id, ρ=m/V )
+})
+
+bot.onText(/(формул)|(скажи).(глубин)|(жидк)*, (msg, match) => {
+	bot.sendMessage(msg.chat.id, `P=ρ∙g∙h`)
+})
+
+bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
+	bot.sendMessage(msg.chat.id, `Fт=mg`)
+})
+
+bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
+	bot.sendMessage(msg.chat.id, `Fa=ρж∙g∙Vт`)
+})
+
+bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
+	bot.sendMessage(msg.chat.id, ``)
+})
+
+bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
+	bot.sendMessage(msg.chat.id, `Давление Р=F/S`)
+})
+
