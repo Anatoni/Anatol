e9a3106e9e0842643a518a7a89a0efbb7b5e9bb8
no <no@no.no>
2019-04-11 15:26:37 +0400

    добавил формулы

--- index.js
+++ index.js
@@ -18,26 +18,26 @@ bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
 })
 
 bot.onText(/(формул)|(скажи).плотн*, (msg, match) => {
-	bot.sendMessage(msg.chat.id, ρ=m/V )
+	bot.sendMessage(msg.chat.id, `ρ=m/V` )
 })
 
 bot.onText(/(формул)|(скажи).(глубин)|(жидк)*, (msg, match) => {
 	bot.sendMessage(msg.chat.id, `P=ρ∙g∙h`)
 })
 
-bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
+bot.onText(/(формул)|(скажи).сил.тяжесь, (msg, match) => {
 	bot.sendMessage(msg.chat.id, `Fт=mg`)
 })
 
-bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
+bot.onText(/(формул)|(скажи).Архимедов.сил.*, (msg, match) => {
 	bot.sendMessage(msg.chat.id, `Fa=ρж∙g∙Vт`)
 })
 
-bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
-	bot.sendMessage(msg.chat.id, ``)
+bot.onText(/(формул)|(скажи).ускорени., (msg, match) => {
+	bot.sendMessage(msg.chat.id, ` a=(υ-υ 0)/t`)
 })
 
-bot.onText(/(формул)|(скажи).*давлени/, (msg, match) => {
-	bot.sendMessage(msg.chat.id, `Давление Р=F/S`)
+bot.onText(/(формул)|(скажи)., (msg, match) => {
+	bot.sendMessage(msg.chat.id, ``)
 })
 
